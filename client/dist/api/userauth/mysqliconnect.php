d81d64c3fd3fd28b59df2cf49299eceedc96f79c
Sudip Baral <pujasudip@gmail.com>
2018-09-29 01:44:31 -0700

    changed a bit

--- client/dist/api/userauth/mysqliconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', 'root', 'final_project', '3306');
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
-?>
